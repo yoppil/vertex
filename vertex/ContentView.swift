de7eac8dfbe4ddfe69170bb9b328d51a85b5cd0b
yxpxi <shonansinjukuline@gmail.com>
2025-11-20 14:11:50 +0900

    Initial Commit

---
+++ vertex/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  vertex
+//
+//  Created by yoppii on 2025/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ vertex/vertexApp.swift
@@ -0,0 +1,17 @@
+//
+//  vertexApp.swift
+//  vertex
+//
+//  Created by yoppii on 2025/11/20.
+//
+
+import SwiftUI
+
+@main
+struct vertexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
